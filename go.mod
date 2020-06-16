ccffd039be6648dab22e6b3ea53c8d5760a9530b
Keftcha <alban.dewilde@gmail.com>
2020-06-16 14:18:33 +0200

    Get the uptime the good way

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/keftcha/lightweight_system_monitor
 
 go 1.14
+
+require golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1
--- lsm.go
+++ lsm.go
@@ -10,7 +10,15 @@ func main() {
 	date := time.Now().Format("2006-01-02 15:04:05")
 	fmt.Printf("\033[01;36m%s\033[0m\n", date)
 
+	// Uptime
+	fmt.Printf("Uptime: %s\n", formatedUptime())
+
+	fmt.Println()
+	// To remove
 	uptime, average := uptimeAndAverage()
 	fmt.Println(uptime)
 	fmt.Println(average)
+
+	fmt.Println()
+
 }
